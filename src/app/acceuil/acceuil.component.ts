aa75961c9e18bd4a6ce3430aab1d74b82c5e347b
samfresh09 <samtou.gnazouyoufei@ipnetinstitute.com>
2023-11-08 18:00:52 +0000

    initalisation de projet angular et creation de component

---
+++ src/app/acceuil/acceuil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceuil-component',
+  templateUrl: './acceuil.component.html',
+  styleUrls: ['./acceuil.component.css']
+})
+export class AcceuilComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
