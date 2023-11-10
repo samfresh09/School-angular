ae13245e8ce12eeb1172b32646900c05e0f8e8e9
samfresh09 <samtou.gnazouyoufei@ipnetinstitute.com>
2023-11-10 11:25:34 +0000

    creation de architecture principale

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css','/src/assets/css/icons.min.css']
 })
 export class BodyComponent {
 
